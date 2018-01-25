89ccbed8bca6a4da61fceece79f3cb50a60fa9f4
nisreen18-meet <nisreenjanah234@gail.com>
2018-01-25 19:55:45 +0200

    haah

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
+	return render_template('home.html')
+
+@app.route("/fanfictions")
+def fanfiction():
+	return render_template('fanfictions.html')
+
+@app.route("/facts")
+def facts():
+	return render_template('facts.html')
+
+@app.route("/about")
+def about():
 	return render_template('about.html')
 if __name__ == "__main__":
     app.run()	
