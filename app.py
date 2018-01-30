54c6db1b509dc9f90c80357ea8800ee3df32aaff
nisreen18-meet <nisreenjanah234@gail.com>
2018-01-30 18:59:27 +0200

    Changed to deploy to heroku

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
 heroku = Heroku(app)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
